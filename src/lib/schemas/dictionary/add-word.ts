46bc666b4b764327d4629c7b4a317629116e1e5f
cel3ntano <adriano2024celentano@gmail.com>
2025-02-22 01:13:41 +0100

    fix: update validation schema for AddWordForm

--- src/lib/schemas/dictionary/add-word.ts
+++ src/lib/schemas/dictionary/add-word.ts
@@ -4,21 +4,21 @@ import { WordCategory } from '@/lib/types/dictionary';
 export const addWordSchema = yup.object().shape({
   en: yup
     .string()
-    .required('Word is required')
-    .min(2, 'Word must be at least 2 characters')
-    .max(50, 'Word must be at most 50 characters')
-    .matches(
-      /^(?!.*--)(?!^-)(?!.*-$)[\p{L}\p{M}\s\-]+$/u,
-      'Please enter a valid word'
-    ),
-  ua: yup
-    .string()
     .required('Translation is required')
     .min(2, 'Translation must be at least 2 characters')
     .max(50, 'Translation must be at most 50 characters')
     .matches(
-      /^(?!.*--)(?!^-)(?!.*-$)[\p{L}\p{M}\s\-]+$/u,
-      'Please enter a valid translation'
+      /\b[A-Za-z'-]+(?:\s+[A-Za-z'-]+)*\b/,
+      'Please enter a valid english translation'
+    ),
+  ua: yup
+    .string()
+    .required('Word is required')
+    .min(2, 'Word must be at least 2 characters')
+    .max(50, 'Word must be at most 50 characters')
+    .matches(
+      /^(?![A-Za-z])[А-ЯІЄЇҐґа-яієїʼ\s]+$/u,
+      'Please enter a valid ukrainian word'
     ),
   category: yup
     .string()
